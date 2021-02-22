4d3ec74c9117f2ebc66cf9d66fee53b3f0f92382
damarinm26 <diegomar26@gmail.com>
2021-02-22 10:58:58 -0500

    Ajustes migración en la nube

--- src/main/java/co/com/segurosalfa/siniestros/repo/ISnrDatoSalidaRepo.java
+++
@@ -1,7 +0,0 @@
-package co.com.segurosalfa.siniestros.repo;
-
-import co.com.segurosalfa.siniestros.entity.SnrDatoSalida;
-
-public interface ISnrDatoSalidaRepo extends IGenericRepo<SnrDatoSalida, Long>{
-
-}
